17ff10642e9e4a7ef21ffa6311395b7b5883d528
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 16:03:03 +0100

    Migrate to version 3.4

--- tests/test_purchase_payment_type.py
+++ tests/test_purchase_payment_type.py
@@ -1,45 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#This file is part of purchase_payment_type module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part of purchase_payment_type module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
+
 class PurchasePaymentTypeTestCase(unittest.TestCase):
-    '''
-    Test PurchasePaymentType module.
-    '''
+    'Test PurchasePaymentType module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('purchase_payment_type')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('purchase_payment_type')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
+
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PurchasePaymentTypeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
